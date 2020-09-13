404bca2bbba5165608472b59d174474d54ccc5b6
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2020-09-13 15:47:22 +0100

    add alt modifier to bottom left

--- keyboards/ergodox_ez/keymaps/erkannt/keymap.c
+++ keyboards/ergodox_ez/keymaps/erkannt/keymap.c
@@ -76,7 +76,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,
         KC_NO, KC_Q,  KC_W,  KC_E,  KC_R,   KC_T,   KC_NO,
         KC_NO, KC_A,  KC_S,  KC_D,  KC_F,   KC_G,
-        KC_NO, GU_Z,  CT_X,  KC_C,  KC_V,   KC_B,   KC_NO,
+        KC_NO, GU_Z,  CT_X,  AL_C,  KC_V,   KC_B,   KC_NO,
         KC_NO, KC_NO, KC_NO, KC_NO, C(KC_A),
                                                       KC_NO,  KC_NO,
                                                               KC_NO,
